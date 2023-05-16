cf222e5440f3e1aaf9fff4fadecb682cfbea02b2
Tony118g <guimaraesantony@gmail.com>
2023-05-16 11:26:49 +0000

    Add styles to the navbar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,12 +1,13 @@
 import React from "react";
 import { Navbar, Container, Nav } from 'react-bootstrap';
 import logo from '../assets/captured-logo.png'
+import styles from '../styles/NavBar.module.css'
 
 const NavBar = () => {
     return (
-        <Navbar expand="md" fixed="top">
+        <Navbar className={styles.NavBar} expand="md" fixed="top">
             <Container>
-                <Navbar.Brand href="#home">
+                <Navbar.Brand className={styles.Logo} href="#home">
                     <img src={logo} alt="Captured logo" height="60"/>
                 </Navbar.Brand>
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
