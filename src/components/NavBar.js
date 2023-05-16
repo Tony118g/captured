82716017e5e7273f7e5cc3d2ea44a54896945517
Tony118g <guimaraesantony@gmail.com>
2023-05-16 10:25:31 +0000

    Add the logo and links to the navbar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,9 +1,26 @@
-import React from 'react'
+import React from "react";
+import { Navbar, Container, Nav } from 'react-bootstrap';
+import logo from '../assets/captured-logo.png'
 
 const NavBar = () => {
-  return (
-    <div>NavBar placeholder</div>
-  )
-}
+    return (
+        <Navbar expand="md" fixed="top">
+            <Container>
+                <Navbar.Brand href="#home">
+                    <img src={logo} alt="Captured logo" height="60"/>
+                </Navbar.Brand>
+                <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                <Navbar.Collapse id="basic-navbar-nav">
+                    <Nav className="ml-auto">
+                        <Nav.Link>Home</Nav.Link>
+                        <Nav.Link>About</Nav.Link>
+                        <Nav.Link>Login</Nav.Link>
+                        <Nav.Link>Signup</Nav.Link>
+                    </Nav>
+                </Navbar.Collapse>
+            </Container>
+        </Navbar>
+    );
+};
 
-export default NavBar
+export default NavBar;
