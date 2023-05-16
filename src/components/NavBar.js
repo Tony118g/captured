5f1d3284113838b499e09726e8c4f3baaa985338
Tony118g <guimaraesantony@gmail.com>
2023-05-16 13:22:29 +0000

    Add styles for active navlinks

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -8,24 +8,24 @@ const NavBar = () => {
     return (
         <Navbar className={styles.NavBar} expand="md" fixed="top">
             <Container>
-                <NavLink to="/">
-                    <Navbar.Brand className={styles.Logo}>
+                <NavLink className={styles.Logo} to="/">
+                    <Navbar.Brand>
                         <img src={logo} alt="Captured logo" height="60" />
                     </Navbar.Brand>
                 </NavLink>
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav">
                     <Nav className="ml-auto">
-                        <NavLink exact to="/">
+                        <NavLink className={styles.NavLink} activeClassName={styles.Active} exact to="/">
                             <i className="fa-solid fa-house"></i>Home
                         </NavLink>
-                        <NavLink to="/about">
+                        <NavLink className={styles.NavLink} activeClassName={styles.Active} to="/about">
                             <i className="fa-solid fa-book-open"></i>About
                         </NavLink>
-                        <NavLink to="/login">
+                        <NavLink className={styles.NavLink} activeClassName={styles.Active} to="/login">
                             <i className="fa-solid fa-right-to-bracket"></i>Login
                         </NavLink>
-                        <NavLink to="/signup">
+                        <NavLink className={styles.NavLink} activeClassName={styles.Active} to="/signup">
                             <i className="fa-solid fa-user-plus"></i>Signup
                         </NavLink>
                     </Nav>
