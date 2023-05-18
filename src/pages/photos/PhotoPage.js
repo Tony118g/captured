68b9fa03d51a515cf67d735c66ab724ec048631d
Tony118g <guimaraesantony@gmail.com>
2023-05-18 13:25:39 +0000

    Add functionality to get a photo from API by id

--- src/pages/photos/PhotoPage.js
+++ src/pages/photos/PhotoPage.js
@@ -1,13 +1,32 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 import Col from "react-bootstrap/Col";
 import Row from "react-bootstrap/Row";
 import Container from "react-bootstrap/Container";
 
 import appStyles from "../../App.module.css";
+import { useParams } from "react-router-dom/cjs/react-router-dom.min";
+import { axiosReq } from "../../api/axiosDefaults";
 
 function PhotoPage() {
-    // Add your logic here
+    const { id } = useParams();
+    const [photo, setPhoto] = useState({ results: [] });
+
+    useEffect(() => {
+        const handleMount = async () => {
+            try {
+                const [{ data: photo }] = await Promise.all([
+                    axiosReq.get(`/photos/${id}`),
+                ]);
+                setPhoto({ results: [photo] });
+                console.log(photo);
+            } catch (err) {
+                console.log(err);
+            }
+        };
+
+        handleMount();
+    }, [id]);
 
     return (
         <Row className="h-100">
@@ -17,7 +36,7 @@ function PhotoPage() {
             </Col>
             <Col md={8} className="d-none d-lg-block p-0 p-lg-2">
                 <p>Popular profiles for mobile</p>
-                <p>Post component</p>
+                <p>Photo post component</p>
                 <Container className={appStyles.Content}>Comments</Container>
             </Col>
         </Row>
