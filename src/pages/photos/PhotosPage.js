3e428fd5824669d479f101bdc277740917650ab5
Tony118g <guimaraesantony@gmail.com>
2023-05-19 09:32:20 +0000

    Add functionality to search for specific photos
    Add a search bar for users to input their search
    Add code to get search results and display them after one second

--- src/pages/photos/PhotosPage.js
+++ src/pages/photos/PhotosPage.js
@@ -6,19 +6,18 @@ import { axiosReq } from "../../api/axiosDefaults";
 import Photo from "./Photo";
 import Asset from "../../components/Asset";
 import appStyles from "../../App.module.css";
-import { Container } from "react-bootstrap";
+import { Container, Form } from "react-bootstrap";
 
 function PhotosPage({ message, filter = "" }) {
     const [photos, setPhotos] = useState({ results: [] });
     const [hasLoaded, setHasLoaded] = useState(false);
     const { pathname } = useLocation();
+    const [query, setQuery] = useState("");
 
     useEffect(() => {
         const fetchPhotos = async () => {
             try {
-                const { data } = await axiosReq.get(
-                    `/photos/?${filter}`
-                );
+                const { data } = await axiosReq.get(`/photos/?${filter}search=${query}`);
                 setPhotos(data);
                 setHasLoaded(true);
             } catch (err) {
@@ -34,7 +33,7 @@ function PhotosPage({ message, filter = "" }) {
         return () => {
             clearTimeout(timer);
         };
-    }, [filter, pathname]);
+    }, [filter, query, pathname]);
 
     return (
         <Row className="h-100">
@@ -45,15 +44,30 @@ function PhotosPage({ message, filter = "" }) {
             <Col md={8} className="p-0 p-lg-2">
                 <p>Post a photo, Feed, Liked photos, Tours for mobile </p>
                 <p>Popular profiles for mobile</p>
+                <i className={`fas fa-search`} />
+                <Form
+                    onSubmit={(event) => event.preventDefault()}
+                >
+                    <Form.Control
+                        value={query}
+                        onChange={(event) => setQuery(event.target.value)}
+                        type="text"
+                        className="mr-sm-2"
+                        placeholder="Search posts"
+                    />
+                </Form>
+
                 {hasLoaded ? (
                     <>
-                    {photos.results.length ? (
-                        photos.results.map(photo => (
-                            <Photo key={photo.id} {...photo} setPhotos={setPhotos} />
-                        ))
-                    ) : (
-                        console.log("show no results message")
-                    )}
+                        {photos.results.length
+                            ? photos.results.map((photo) => (
+                                  <Photo
+                                      key={photo.id}
+                                      {...photo}
+                                      setPhotos={setPhotos}
+                                  />
+                              ))
+                            : console.log("show no results message")}
                     </>
                 ) : (
                     <Container className={appStyles.Content}>
