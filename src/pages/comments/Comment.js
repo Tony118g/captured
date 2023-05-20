b15f705ee05d551ad06dfd3f77b51e439027563e
Tony118g <guimaraesantony@gmail.com>
2023-05-20 12:05:52 +0000

    Add styles to the comments

--- src/pages/comments/Comment.js
+++ src/pages/comments/Comment.js
@@ -2,6 +2,7 @@ import React from "react";
 import { Link } from "react-router-dom";
 import Avatar from "../../components/Avatar";
 import { Media } from "react-bootstrap";
+import styles from "../../styles/Comment.module.css";
 
 const Comment = (props) => {
     const { profile_id, profile_image, owner, updated_at, content } = props;
@@ -13,10 +14,10 @@ const Comment = (props) => {
                 <Link to={`/profiles/${profile_id}`}>
                     <Avatar src={profile_image} />
                 </Link>
-                <Media.Body>
-                    <span>{owner}</span>
-                    <span>{updated_at}</span>
-                    <p>{content}</p>
+                <Media.Body className="align-self-center ml-2">
+                    <span className={styles.Owner}>{owner}</span>
+                    <span className={styles.Date}>{updated_at}</span>
+                    <p className="mb-0 mt-2">{content}</p>
                 </Media.Body>
             </Media>
         </div>
