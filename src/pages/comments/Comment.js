af57a1135195a7f8826791fcde5cc85ea948e3e7
Tony118g <guimaraesantony@gmail.com>
2023-05-20 12:23:20 +0000

    Add a edit/delete dropdown menu to comments

--- src/pages/comments/Comment.js
+++ src/pages/comments/Comment.js
@@ -3,9 +3,14 @@ import { Link } from "react-router-dom";
 import Avatar from "../../components/Avatar";
 import { Media } from "react-bootstrap";
 import styles from "../../styles/Comment.module.css";
+import { EditDeleteDropdown } from "../../components/EditDeleteDropdown";
+import { useCurrentUser } from "../../contexts/CurrentUserContext";
 
 const Comment = (props) => {
     const { profile_id, profile_image, owner, updated_at, content } = props;
+
+    const currentUser = useCurrentUser();
+    const is_owner = currentUser?.username === owner;
 
     return (
         <div>
@@ -19,6 +24,9 @@ const Comment = (props) => {
                     <span className={styles.Date}>{updated_at}</span>
                     <p className="mb-0 mt-2">{content}</p>
                 </Media.Body>
+                {is_owner && (
+                    <EditDeleteDropdown handleEdit={() => {}} handleDelete={() => {}}/>
+                )}
             </Media>
         </div>
     );
