26fd650354399a6f82efb69544009f7a27495200
Tony118g <guimaraesantony@gmail.com>
2023-05-24 11:21:57 +0000

    Add functionality to display all tours

--- src/pages/tours/ToursPage.js
+++ src/pages/tours/ToursPage.js
@@ -1,8 +1,28 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Col from "react-bootstrap/Col";
 import Row from "react-bootstrap/Row";
+import { useLocation } from "react-router-dom";
+import { axiosReq } from "../../api/axiosDefaults";
+import Tour from "./Tour";
 
 function ToursPage({ message, filter = "" }) {
+    const [tours, setTours] = useState({ results: [] });
+    const [hasLoaded, setHasLoaded] = useState(false);
+    const { pathname } = useLocation();
+
+    useEffect(() => {
+        const fetchTours = async () => {
+            try {
+                const { data } = await axiosReq.get(`/tours/?${filter}`);
+                setTours(data);
+                setHasLoaded(true);
+            } catch (err) {
+                console.log(err);
+            }
+        };
+        setHasLoaded(false);
+        fetchTours();
+    }, [filter, pathname]);
 
     return (
         <Row className="h-100">
@@ -13,7 +33,21 @@ function ToursPage({ message, filter = "" }) {
             <Col md={8} className="p-0 p-lg-2">
                 <p>Post a photo, Feed, Liked photos, Tours for mobile </p>
                 <p>Popular profiles for mobile</p>
-                <p>List of tours goes here</p>
+                {hasLoaded ? (
+                    <>
+                        {tours.results.length
+                            ? tours.results.map((tour) => (
+                                  <Tour
+                                      key={tour.id}
+                                      {...tour}
+                                      setTours={setTours}
+                                  />
+                              ))
+                            : console.log("show no results message")}
+                    </>
+                ) : (
+                    console.log("show loading spinner")
+                )}
             </Col>
         </Row>
     );
