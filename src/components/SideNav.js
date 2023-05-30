94218e97c652e2ee33805f9887679236f25edb5e
Tony118g <guimaraesantony@gmail.com>
2023-05-30 16:17:23 +0000

    Change incorrect class attributes to be className

--- src/components/SideNav.js
+++ src/components/SideNav.js
@@ -30,15 +30,15 @@ const SideNav = ({ mobile }) => {
                 <p className={mobile ? "d-none" : "d-inline"}>Post photo</p>
             </Link>
             <Link to="/liked" className={mobile ? "col-3" : "d-block pt-2"}>
-                <i class="fa-solid fa-heart"></i>
+                <i className="fa-solid fa-heart"></i>
                 <p className={mobile ? "d-none" : "d-inline"}>Liked photos</p>
             </Link>
             <Link to="/feed" className={mobile ? "col-3" : "d-block pt-2"}>
-                <i class="fa-solid fa-rss"></i>
+                <i className="fa-solid fa-rss"></i>
                 <p className={mobile ? "d-none" : "d-inline"}>Feed</p>
             </Link>
             <Link to="/tours" className={mobile ? "col-3" : "d-block pt-2"}>
-                <i class="fa-solid fa-camera-retro"></i>
+                <i className="fa-solid fa-camera-retro"></i>
                 <p className={mobile ? "d-none" : "d-inline"}>Tours</p>
             </Link>
         </Container>
