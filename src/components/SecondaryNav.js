df2039cce8f3a3a97e52aa66e56bab8537333237
Tony118g <guimaraesantony@gmail.com>
2023-05-31 16:11:34 +0000

    Add styles for SecondaryNav.js on smaller screens

--- src/components/SecondaryNav.js
+++ src/components/SecondaryNav.js
@@ -1,48 +1,60 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import { Container } from "react-bootstrap";
+import { Container, Nav } from "react-bootstrap";
 import appStyles from "../App.module.css";
+import styles from "../styles/SecondaryNav.module.css";
 import { useCurrentUser } from "../contexts/CurrentUserContext";
 
-const SideNav = ({ mobile }) => {
+const SecondaryNav = ({ mobile }) => {
     const currentUser = useCurrentUser();
 
     return (
-        <Container fluid
-            className={`${appStyles.Content} mt-2 ${
-                mobile && "d-lg-none text-center mt-3"
-            }`}
-        >
-            {currentUser?.is_admin_user && (
-                <Link
-                    to="/tours/create"
-                    className={mobile ? "col-3" : "d-block pt-2"}
-                >
-                    <i className="fa-regular fa-plus"></i>
-                    <p className={mobile ? "d-none" : "d-inline"}>Add a tour</p>
-                </Link>
-            )}
-            <Link
-                to="/photos/create"
-                className={mobile ? "col-3" : "d-block pt-2"}
+        <Nav className={`p-0 ${mobile && styles.MobileNav}`}>
+            <Container
+                fluid
+                className={`${appStyles.Content} mt-2 ${
+                    mobile &&
+                    "d-lg-none text-center mt-3 d-flex justify-content-around"
+                }`}
             >
-                <i className="fa-regular fa-plus"></i>
-                <p className={mobile ? "d-none" : "d-inline"}>Post photo</p>
-            </Link>
-            <Link to="/liked" className={mobile ? "col-3" : "d-block pt-2"}>
-                <i className="fa-solid fa-heart"></i>
-                <p className={mobile ? "d-none" : "d-inline"}>Liked photos</p>
-            </Link>
-            <Link to="/feed" className={mobile ? "col-3" : "d-block pt-2"}>
-                <i className="fa-solid fa-rss"></i>
-                <p className={mobile ? "d-none" : "d-inline"}>Feed</p>
-            </Link>
-            <Link to="/tours" className={mobile ? "col-3" : "d-block pt-2"}>
-                <i className="fa-solid fa-camera-retro"></i>
-                <p className={mobile ? "d-none" : "d-inline"}>Tours</p>
-            </Link>
-        </Container>
+                {currentUser?.is_admin_user && (
+                    <Link
+                        to="/tours/create"
+                        className={!mobile && "d-block pt-2"}
+                    >
+                        <i className="fa-regular fa-plus"></i>
+                        <p className={mobile ? styles.SmallText : "d-inline"}>
+                            Add a tour
+                        </p>
+                    </Link>
+                )}
+                <Link to="/photos/create" className={!mobile && "d-block pt-2"}>
+                    <i className="fa-regular fa-plus"></i>
+                    <p className={mobile ? styles.SmallText : "d-inline"}>
+                        Post photo
+                    </p>
+                </Link>
+                <Link to="/liked" className={!mobile && "d-block pt-2"}>
+                    <i className="fa-solid fa-heart"></i>
+                    <p className={mobile ? styles.SmallText : "d-inline"}>
+                        Liked photos
+                    </p>
+                </Link>
+                <Link to="/feed" className={!mobile && "d-block pt-2"}>
+                    <i className="fa-solid fa-rss"></i>
+                    <p className={mobile ? styles.SmallText : "d-inline"}>
+                        Feed
+                    </p>
+                </Link>
+                <Link to="/tours" className={!mobile && "d-block pt-2"}>
+                    <i className="fa-solid fa-camera-retro"></i>
+                    <p className={mobile ? styles.SmallText : "d-inline"}>
+                        Tours
+                    </p>
+                </Link>
+            </Container>
+        </Nav>
     );
 };
 
-export default SideNav;
+export default SecondaryNav;
