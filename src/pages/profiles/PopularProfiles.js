11835e80f9eda89120e8a8a4744b9889c52c371b
Tony118g <guimaraesantony@gmail.com>
2023-06-06 11:20:23 +0000

    Change font family on navbar and popular profiles

--- src/pages/profiles/PopularProfiles.js
+++ src/pages/profiles/PopularProfiles.js
@@ -18,7 +18,7 @@ const PopularProfiles = ({ mobile }) => {
         >
             {popularProfiles.results.length ? (
                 <>
-                    <h5 className={mobile ? "mb-1" : undefined}>Popular profiles</h5>
+                    <h5 className={`${styles.Heading} ${mobile ? "mb-1" : undefined}`}>Popular profiles</h5>
                     {mobile ? (
                         <div className="d-flex justify-content-around my-0">
                             {popularProfiles.results
