f9d9a7db6d7dd5874ab8d2afd3b8dca9762af9e3
Tony118g <guimaraesantony@gmail.com>
2023-06-07 12:59:56 +0000

    Set up the project to create unit tests

---
+++ src/mocks/handlers.js
@@ -0,0 +1,22 @@
+const baseURL = "https://captured-drf-api.herokuapp.com/";
+
+export const handlers = [
+    rest.get(`${baseURL}dj-rest-auth/user/`, (req, res, ctx) => {
+        return res(
+            ctx.json({
+                pk: 1,
+                username: "admin",
+                email: "",
+                first_name: "",
+                last_name: "",
+                profile_id: 1,
+                profile_image:
+                    "https://res.cloudinary.com/dk6v9nvmq/image/upload/v1/media/../default_profile_gaddut",
+                is_admin_user: true,
+            })
+        );
+    }),
+    rest.post(`${baseURL}dj-rest-auth/logout/`, (req, res, ctx) => {
+        return res(ctx.status(200));
+    }),
+];
